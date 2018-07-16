300bf957afcba7ecb9852580b7f6d1a178aa19be
SyahrulRomadoni <syahrulromadoni9988@gmail.com>
2018-07-16 23:08:58 +0700

    nyoba pc 1

--- 1/halaman/mod_anggota/detail_anggota.php
+++ 1/halaman/mod_anggota/detail_anggota.php
@@ -4,7 +4,7 @@ include "../config/koneksi.php";
 
 $id=$_GET['id'];
 
-$query = "SELECT * FROM anggota WHERE id='$id'";
+$query = "SELECT anggota.*, user.*FROM anggota, user WHERE anggota.id=user.id_anggota";
 $hasil = mysqli_query($conn, $query);
 $tampil = mysqli_num_rows($hasil);
 
